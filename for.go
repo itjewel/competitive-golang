29370e7159a9d8d86ed6ca341fc99ef27f30390d
Md.Jewel Mia <md.jewelmia52@gmail.com>
2025-05-02 19:00:14 +0600

    added some conditional

--- for.go
+++ for.go
@@ -1,9 +1,10 @@
+// normal for
 for i:0; i<10; i++ {
 	fmt.Println(i)
 }
 
 // only condition
-
+i := 0
 for i<5 {
 	fmt.Println(i)
 	i++
@@ -15,8 +16,32 @@ for {
 	Println("infinite loop")
 }
 
+i := 0
 // no initial values
 for ; i<10; {
 	fmt.Println(i)
 	i++
+}
+
+// Like while
+
+i: 0;
+for i<10 {
+	fmt.Println(i)
+	i++
+}
+
+// If with a short statement
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
 }
