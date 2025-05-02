4476db8afa91551182b166c5ba42e25af7cef60a
Md.Jewel Mia <md.jewelmia52@gmail.com>
2025-05-02 10:58:17 +0600

    loop added

--- for.go
+++ for.go
@@ -1,3 +1,22 @@
 for i:0; i<10; i++ {
 	fmt.Println(i)
 }
+
+// only condition
+
+for i<5 {
+	fmt.Println(i)
+	i++
+}
+
+// infinite loop
+
+for {
+	Println("infinite loop")
+}
+
+// no initial values
+for ; i<10; {
+	fmt.Println(i)
+	i++
+}
